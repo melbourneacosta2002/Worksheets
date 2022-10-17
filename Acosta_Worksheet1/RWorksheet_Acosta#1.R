196fd90afdd352872f96dcc3026c19f44e6ad10b
Melbourne Acosta <melbznaruto@gmail.com>
2022-10-17 22:44:01 +0800

    worksheets

---
+++ Acosta_Worksheet1/RWorksheet_Acosta#1.R
@@ -0,0 +1,36 @@
+age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,51, 35, 24, 33, 41)
+age
+data.frame(age)
+pro_age <- 1/age
+pro_age
+new_age <- c(age, 0, age)
+new_age
+sort(age)
+min(age)
+max(age)
+data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
+data
+data * 2
+seq(1:100)
+seq(20,60)
+mean(20:60)
+sum(51:91)
+seq(1:1000)
+data.frame(1:100)
+data.frame(20:60)
+data.frame(mean(20:60))
+data.frame(sum(51:91))
+max(1:10)
+filter(function(i){all(i%%c(3, 5, 7) != 0)}, seq(100))
+filter(function(i){all(i%%c(3, 5, 7) != 0)}, seq(100))
+seq(100:1)
+sum((1:25)[((1:25)%%3==0) | ((1:25)%%5==0)])
+data.frame(1:100)
+{ x <- 0+ x + 5 + }
+score <- c(72, 86, 92, 63, 88, 89, 91, 92, 75, 75, 77)
+a = c(1, 2, NA, 4, NA, 6, 7)
+print(a, na.print="-999")
+name = readline(prompt="Input your name ") 
+age = readline(prompt="Input your age: ")
+print(paste("My name is",name, "and I am",age ,"years old.")) 
+print(R.version.string)
